0d302b8d998beb4c3cf0a23d828f3e85438bfd61
Christopher Tate <cwt@usf.edu>
2022-05-20 21:48:48 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using IS6225_ModelBinding.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IS6225_ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {            
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            Register member = new Register();
+            return View(member);
+        }
+        [HttpPost]
+        public IActionResult SignUp(Register member)
+        {
+            return View(member);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,11 @@
+﻿namespace IS6225_ModelBinding.Models
+{
+    public class Register
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
